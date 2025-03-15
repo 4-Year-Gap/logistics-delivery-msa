4b3acde8173b9e7ee64a1ff61ee39048c0de12f4
leeseowoo <leeseowoo.kr@gmail.com>
2025-03-15 15:43:10 +0900

    [refactor] @Comment 애너테이션을 적용하여 컬럼 주석 추가

--- user/src/main/java/com/springcloud/client/user/domain/User.java
+++ user/src/main/java/com/springcloud/client/user/domain/User.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 
 import java.util.UUID;
 
@@ -18,18 +19,23 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Comment("PK")
     private UUID userId;
 
     @Column(nullable = false, unique = true)
+    @Comment("로그인 ID")
     private String username;
 
     @Column(nullable = false)
+    @Comment("비밀번호")
     private String password;
 
     @Column(nullable = false)
+    @Comment("슬랙 Email")
     private String slackId;
 
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
+    @Comment("사용자 권한")
     private UserRole role;
 }

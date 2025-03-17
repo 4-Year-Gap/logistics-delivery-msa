7a75326fd96d795eff41361c6da6405b81a7c63b
kylim99 <zxzx05050@naver.com>
2025-03-17 19:42:37 +0900

    [feat] client test 푸시

--- order/src/main/java/com/spring_cloud/eureka/client/order/infrastructure/config/KafkaConfig.java
+++ order/src/main/java/com/spring_cloud/eureka/client/order/infrastructure/config/KafkaConfig.java
@@ -1,17 +1,17 @@
 package com.spring_cloud.eureka.client.order.infrastructure.config;
 
 
-
-
-
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,14 +20,30 @@ import java.util.Map;
 public class KafkaConfig {
 
 
+
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.password}")
+    private String password;
+
+    @Value("${spring.kafka.user-name}")
+    private String username;
     @Bean
     public ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
 
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+        configProps.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+        String jaasConfig = String.format(
+                "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"%s\" password=\"%s\";",
+                username, password);
+        configProps.put(SaslConfigs.SASL_JAAS_CONFIG, jaasConfig);
+        return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
@@ -39,4 +55,6 @@ public class KafkaConfig {
     public KafkaTemplate<String, String> createEventKafkaTemplate(){
         return new KafkaTemplate<String,String>(producerFactory());
     }
+
+
 }

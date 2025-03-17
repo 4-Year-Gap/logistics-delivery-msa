0cbc1b34bef0cee3b59022834b56dee0a0187f63
jyooung <cherish.yooung@gmail.com>
2025-03-17 22:47:34 +0900

    [feat] 업체 등록 구현

---
+++ company/src/main/java/com/springcloud/company/company/repository/CompanyRepository.java
@@ -0,0 +1,14 @@
+package com.springcloud.company.company.repository;
+
+import com.springcloud.company.company.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, UUID> {
+    Optional<Company> findByUserId(UUID userId);
+
+}

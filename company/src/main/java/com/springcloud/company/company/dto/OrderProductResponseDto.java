cb63b379382c748bc6cbdb46cd40ea7b14c29e26
jyooung <cherish.yooung@gmail.com>
2025-03-18 14:21:05 +0900

    [feat] 주문 서버에서 상품, 업체 조회 기능 - 주문 서버에서 업체 서버로 요청을 보내며 recevingCompanyId, productId, quantity를 넘기면 업체서비스에서 상품객체에 접근하여 productId, startHub, endHub를 반환한다. - 유레카 서버는 연결 안된 상태

---
+++ company/src/main/java/com/springcloud/company/company/dto/OrderProductResponseDto.java
@@ -0,0 +1,21 @@
+package com.springcloud.company.company.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+public class OrderProductResponseDto {
+    private UUID productId;
+    private UUID startHub;
+    private UUID endHub;
+
+    public OrderProductResponseDto(UUID startHub, UUID id, UUID endHub) {
+        this.productId = id;
+        this.startHub = startHub;
+        this.endHub = endHub;
+    }
+}

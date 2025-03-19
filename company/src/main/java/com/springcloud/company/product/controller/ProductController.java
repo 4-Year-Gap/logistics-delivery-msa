afaf62b5b44794513d8b17693237c307b424e76d
jyooung <cherish.yooung@gmail.com>
2025-03-19 12:43:57 +0900

    [feat] 주문 서버에서 재고 차감 요청 api 추가 - feignClient 적용 (build.gradle, CompanyApplication.java) - productService.updateStock() 구현

--- company/src/main/java/com/springcloud/company/product/controller/ProductController.java
+++ company/src/main/java/com/springcloud/company/product/controller/ProductController.java
@@ -2,12 +2,12 @@ package com.springcloud.company.product.controller;
 
 import com.springcloud.company.product.dto.ProductRequestDto;
 import com.springcloud.company.product.dto.ProductResponseDto;
+import com.springcloud.company.product.dto.UpdateProductStockRequestDto;
+import com.springcloud.company.product.dto.UpdateProductStockResponseDto;
 import com.springcloud.company.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Description;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -26,5 +26,11 @@ public class ProductController {
         UUID userId = UUID.fromString("1512a8fe-f5c3-4ac8-998e-84c2fc2bab7d"); //UUID.randomUUID();  TODO: 임시 데이터
         //응답 보내기
         return productService.createProduct(requestDto,userId);
+    }
+    @GetMapping("/stock")
+    @Description("상품 재고 차감")
+    public UpdateProductStockResponseDto updateProductStock(@RequestBody UpdateProductStockRequestDto RequestDto
+    ){
+        return productService.updateStock(RequestDto);
     }
 }
--- company/src/main/java/com/springcloud/company/product/service/ProductService.java
+++ company/src/main/java/com/springcloud/company/product/service/ProductService.java
@@ -2,14 +2,19 @@ package com.springcloud.company.product.service;
 
 import com.springcloud.company.company.entity.Company;
 import com.springcloud.company.company.repository.CompanyRepository;
+import com.springcloud.company.company.service.CompanyService;
 import com.springcloud.company.product.dto.ProductRequestDto;
 import com.springcloud.company.product.dto.ProductResponseDto;
+import com.springcloud.company.product.dto.UpdateProductStockRequestDto;
+import com.springcloud.company.product.dto.UpdateProductStockResponseDto;
 import com.springcloud.company.product.entity.Product;
 import com.springcloud.company.product.repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -18,6 +23,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     private final CompanyRepository companyRepository;
+    private final CompanyService companyService;
 
 
     @Transactional
@@ -37,4 +43,35 @@ public class ProductService {
         productRepository.save(product);
         return new ProductResponseDto(product);
     }
+
+    @Transactional
+    public UpdateProductStockResponseDto updateStock(UpdateProductStockRequestDto requestDto) {
+        //상품의 ID로 Company불러옴
+        Company company = companyService.getCompanyByProductId(requestDto.getProductId());
+
+//        // 업체 1: 상품 N 이라서 productId에 맞는 상품을 불러옴
+//        Product product = company.getProducts().stream()
+//                .filter( p -> p.getId().equals(productId))
+//                .findAny()
+//                .orElseThrow();
+//
+        //Optional<Product>를 먼저 저장하면 디버깅과 유지보수가 쉬워짐
+        Optional<Product> optionalProduct = company.getProducts().stream()
+                .filter(p -> p.getId().equals(requestDto.getProductId()))
+                .findAny();
+
+        Product product = optionalProduct.orElseThrow(() -> new NoSuchElementException("Product not found"));
+
+        // 상품 도메인 재고차감 로직
+        product.deduct(requestDto.getStock());
+
+        // DB 반영
+        productRepository.save(product);
+
+        return new UpdateProductStockResponseDto(product);
+
+    }
+
+
+
 }

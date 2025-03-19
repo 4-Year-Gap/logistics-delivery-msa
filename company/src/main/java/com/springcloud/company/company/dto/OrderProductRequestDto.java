8eff7aa676be9cb69ea786030471e9382313d4b5
jyooung <109530847+jyooung@users.noreply.github.com>
2025-03-19 11:47:05 +0900

    Update OrderProductRequestDto.java
    Update receiving 오타 업데이트
    - dto컨트롤러 오타 수정

--- company/src/main/java/com/springcloud/company/company/dto/OrderProductRequestDto.java
+++ company/src/main/java/com/springcloud/company/company/dto/OrderProductRequestDto.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 @Getter
 @AllArgsConstructor
 public class OrderProductRequestDto {
-    private UUID recevingCompanyId;
+    private UUID receivingCompanyId;
     private UUID productId;
     private Integer quantity;
 }

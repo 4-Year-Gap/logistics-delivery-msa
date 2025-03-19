462b0311acead0772789da4c5805b55dbc59b78f
kylim99 <zxzx05050@naver.com>
2025-03-19 12:40:38 +0900

    [fix] kafka 수정

--- order/src/main/java/com/spring_cloud/eureka/client/order/config/KafkaConfig.java
+++ order/src/main/java/com/spring_cloud/eureka/client/order/config/KafkaConfig.java
@@ -1,10 +1,12 @@
 package com.spring_cloud.eureka.client.order.config;
 
 
+
+import com.spring_cloud.eureka.client.order.infrastructure.client.dto.OrderCreateEvent;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SaslConfigs;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +23,6 @@ public class KafkaConfig {
 
 
 
-
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
@@ -31,12 +32,12 @@ public class KafkaConfig {
     @Value("${spring.kafka.user-name}")
     private String username;
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, OrderCreateEvent> producerFactory() {
 
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, OrderCreateEvent.class);
         configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
         configProps.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
         String jaasConfig = String.format(
@@ -47,13 +48,13 @@ public class KafkaConfig {
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<String, OrderCreateEvent> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 
     @Bean
-    public KafkaTemplate<String, String> createEventKafkaTemplate(){
-        return new KafkaTemplate<String,String>(producerFactory());
+    public KafkaTemplate<String, OrderCreateEvent> createEventKafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
     }
 
 
--- order/src/main/java/com/spring_cloud/eureka/client/order/infrastructure/client/dto/OrderCreateEvent.java
+++ order/src/main/java/com/spring_cloud/eureka/client/order/infrastructure/client/dto/OrderCreateEvent.java
@@ -2,23 +2,29 @@ package com.spring_cloud.eureka.client.order.infrastructure.client.dto;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serializer;
 
+import java.io.Serializable;
+import java.util.Map;
 import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
-public class OrderCreateEvent {
+@Builder
+public class OrderCreateEvent implements Serializer {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     private UUID orderId;
     private UUID startHub;
     private UUID endHub;
     private UUID productId;
     private Integer productQuantity;
+
 
     public static OrderCreateEvent create(UUID orderId, UUID startHub, UUID endHub, UUID productId, Integer productQuantity) {
         return new OrderCreateEvent(
@@ -31,13 +37,15 @@ public class OrderCreateEvent {
     }
 
 
-    public String toJson() {
-        ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    public byte[] serialize(String s, Object data) {
         try {
-            return objectMapper.writeValueAsString(this);
+            if (data == null) {
+                return null;
+            }
+            return objectMapper.writeValueAsBytes(data);
         } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+            throw new RuntimeException("Error serializing OrderCreateEvent", e);
         }
     }
 

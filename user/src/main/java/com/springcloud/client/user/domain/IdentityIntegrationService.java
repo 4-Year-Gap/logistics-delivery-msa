65d15ec9eef01aece80a8beadc89dd48ded2838c
leeseowoo <leeseowoo.kr@gmail.com>
2025-03-22 15:38:08 +0900

    [refactor] 키 이름 변경

--- user/src/main/java/com/springcloud/client/user/domain/IdentityIntegrationService.java
+++ user/src/main/java/com/springcloud/client/user/domain/IdentityIntegrationService.java
@@ -12,13 +12,13 @@ import java.util.concurrent.TimeUnit;
 public class IdentityIntegrationService {
 
     private final RedisTemplate<String, IdentityIntegrationDto> redisTemplate;
-    private static final String KEY_PREFIX = "identityIntegrationCache:";
+    private static final String HASH_TABLE_KEY = "identityIntegrationCache";
 
     public void addIdentity(IdentityIntegrationDto dto) {
-        String key = KEY_PREFIX + dto.getUserId().toString();
+        String fieldKey = dto.getUserId().toString();
 
         // Redis에 기존 데이터가 있는지 확인
-        IdentityIntegrationDto existingData = (IdentityIntegrationDto) redisTemplate.opsForHash().get("identityIntegrationCache", key);
+        IdentityIntegrationDto existingData = (IdentityIntegrationDto) redisTemplate.opsForHash().get("identityIntegrationCache", fieldKey);
 
         if (existingData != null) {
             // 기존 데이터가 있으면 업데이트
@@ -27,10 +27,10 @@ public class IdentityIntegrationService {
             if (dto.getOrderId() != null) existingData.setOrderId(dto.getOrderId());
             if (dto.getDeliveryId() != null) existingData.setDeliveryId(dto.getDeliveryId());
 
-            redisTemplate.opsForValue().set(key, existingData, 3, TimeUnit.DAYS);
+            redisTemplate.opsForValue().set(fieldKey, existingData, 3, TimeUnit.DAYS);
         } else {
             // 없으면 새로 저장
-            redisTemplate.opsForHash().put("identityIntegrationCache", key, dto);
+            redisTemplate.opsForHash().put(HASH_TABLE_KEY, fieldKey, dto);
         }
     }
 }

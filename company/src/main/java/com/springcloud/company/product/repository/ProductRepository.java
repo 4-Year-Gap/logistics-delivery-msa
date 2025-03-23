ebf79904323917784aef2564090ad768c95aa840
jyooung <cherish.yooung@gmail.com>
2025-03-23 20:21:03 +0900

    [feat] 업체, 상품 전체 조회 시 Search 기능 추가, Kafka 에서 updateStock 호출 - keyword 파라미터 추가, 불필요한 메서드 삭제 - Query 통해서 키워드 검색 - keyword 전달 값 없으면 전체 조회

--- company/src/main/java/com/springcloud/company/product/repository/ProductRepository.java
+++ company/src/main/java/com/springcloud/company/product/repository/ProductRepository.java
@@ -2,10 +2,15 @@ package com.springcloud.company.product.repository;
 
 import com.springcloud.company.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    @Query("SELECT p FROM Product p WHERE " +
+            "(:keyword IS NULL OR p.productName LIKE %:keyword%)")
+    List<Product> searchProducts(String keyword);
 }
